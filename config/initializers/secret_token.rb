ef49e98bb76b9c1b0626bb8aae031a17bad71784
c0wboigreas3 <dnelems21@gmail.com>
2012-06-17 00:09:34 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mycereal::Application.config.secret_token = 'e43bf510661f56e86b06d84f04f6dc876e40907f5a55cd8f98625b91658844398ad772a7b4913f6c00384f39e2c2172c1e54a10824996d8453153fa4ef94aed2'
---
+++ generate/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'd539a5d83fe74a45f25da1620487c24ffcfc7f0799ef0ef769bf33ba070e97e13305e1f0d1cd2a24fcc1bfcfac8a7401cfe4baa0ec6eefdd248aa0ad803bf26e';
